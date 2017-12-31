35a8537d249e2f03e607b0753d5058ef556b9e5c
SudoUser <vivekmalhotra004@gmail.com>
2017-12-31 13:26:48 -0700

    Heroku version for node.js project - completed (moving to static development of webpage)

--- app.js
+++ app.js
@@ -45,6 +45,6 @@ app.get("/express-entry-PNP", function(req, res){
     res.render("express_entry_PNP.ejs");
 });
 
-app.listen(process.env.PORT, process.env.IP, function(req, res){
+app.listen('3000', process.env.IP, function(req, res){
     console.log("Server started...")
 });
